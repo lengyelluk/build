23fccbaaa89fd9b9bdfc4794495bb8da4f2470c8
lengyelluk <lengyel.luk@gmail.com>
2020-03-03 22:51:37 +0100

    URL changed to heroku

---
+++ src/build/precache-manifest.a309ff345d2063d8c32ffd3224427605.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0baf0f4f6b5bd0972d474b24194183a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acf95077767d5c88c59d",
+    "url": "/static/css/main.da5c48cc.chunk.css"
+  },
+  {
+    "revision": "c27aefb83e83b126382f",
+    "url": "/static/js/2.b06928bf.chunk.js"
+  },
+  {
+    "revision": "48b576eb903785e5ed041e8d343a5ba2",
+    "url": "/static/js/2.b06928bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acf95077767d5c88c59d",
+    "url": "/static/js/main.03a4597f.chunk.js"
+  },
+  {
+    "revision": "fe718a7a4d64a547c057",
+    "url": "/static/js/runtime-main.6fe874b4.js"
+  },
+  {
+    "revision": "06ef1174cc259b43366e8c85e419a620",
+    "url": "/static/media/findRoom.06ef1174.jpg"
+  },
+  {
+    "revision": "dfb6eae288d8680f16277687263fbd01",
+    "url": "/static/media/flat.dfb6eae2.jpg"
+  },
+  {
+    "revision": "30e447f24085929adfa9c80be9e199b2",
+    "url": "/static/media/logo.30e447f2.png"
+  },
+  {
+    "revision": "c1b5f7b0b79b040eeca6a73208f7d73a",
+    "url": "/static/media/rentOutRoom.c1b5f7b0.jpg"
+  }
+]);
--- src/build/service-worker.js
+++ src/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5015199996a6c385cc485fe43ecacaf.js"
+  "/precache-manifest.a309ff345d2063d8c32ffd3224427605.js"
 );
 
 self.addEventListener('message', (event) => {
